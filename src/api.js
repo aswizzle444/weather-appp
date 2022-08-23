30ee8af159444ba6af54d36ccecf94c4f60bca37
aswizzle444 <110160899+aswizzle444@users.noreply.github.com>
2022-08-23 15:10:56 -0400

    Initial commit

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+    method: "GET",
+    headers: {
+        "X-RapidAPI-Key": "226fd2c54dmshc50cafc53bc2852p129b71jsn73e7dc843fed",
+        "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com"
+    }
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "139d33238d7dfccfeba270325e6f1611";
